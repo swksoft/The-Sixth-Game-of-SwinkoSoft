bfde9c9e9f1b5b042d4195a8a82383cde9977b76
swksoft <esparzapatricio95@gmail.com>
2024-03-17 16:10:56 -0300

    a

--- scenes/map_08.gd
+++ scenes/map_08.gd
@@ -1,5 +1,7 @@
 extends Level
 
 func next_level():
+	$Node2D.visible = true
+	$Player.visible = false
 	var scene: String = "res://scenes/map_%s.tscn" % next_map
 	TransitionLayer.change_scene("res://scenes/main_menu.tscn", 7, true)
